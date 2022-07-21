6dc242e3fe23b3410b440ca1d090db5cd6e4acee
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2022-07-21 16:38:53 +0300

    Done some great stuff

--- src/main/java/com/alperen/streammod/App.java
+++ src/main/java/com/alperen/streammod/App.java
@@ -1,43 +1,36 @@
 package com.alperen.streammod;
-import java.util.concurrent.TimeUnit;
 
-import org.freedesktop.gstreamer.Gst;
-import org.freedesktop.gstreamer.Pipeline;
-import org.freedesktop.gstreamer.Version;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
-/**
- * Hello world!
- *
- */
+import org.bytedeco.javacv.FFmpegLogCallback;
+
 public class App 
 {
-	private static Pipeline pipeline1;
-	private static Pipeline pipeline2;
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Gst.init(Version.BASELINE, "steammod", args);
+        try {
+        	//GSTTest.StreamDesktop(args); //Stream desktop to RTP Code
+        	FFmpegLogCallback.set();
+        	//FFMpegTest.SaveStreamToFile("/home/nycrera/recording.mp4", "High");
+            /*Runnable runnable = () -> { 
+    			try {
+					FFMpegTest.DisplayStream();
+				} catch (Exception e) {
+					e.printStackTrace();
+				} // Show RTP stream code
+            };
+            Thread t = new Thread(runnable);
+            t.start();
+            */
+        	FFMpegTest.StreamVideo("/home/nycrera/recording.mp4","127.0.0.1","1234");
 
-        /**
-         * Use Gst.parseLaunch() to create a pipeline from a GStreamer string
-         * definition. This method returns Pipeline when more than one element
-         * is specified.
-         */
-        pipeline1 = (Pipeline) Gst.parseLaunch("udpsrc port=1234 ! application/x-rtp,encoding-name=H264,payload=96 ! rtph264depay ! avdec_h264 ! xvimagesink");
-        pipeline2 = (Pipeline) Gst.parseLaunch("ximagesrc ! video/x-raw,framerate=30/1 ! timeoverlay ! videoconvert ! x264enc ! video/x-h264,profile=baseline ! h264parse config-interval=-1 ! rtph264pay pt=96 config-interval=-1 ! udpsink host=127.0.0.1 port=1234 sync=false");
-        /**
-         * Start the pipeline.
-         */
-        pipeline2.play(); // Start the stream
-        pipeline1.play(); // Read on the stream and display
-        /**
-         * GStreamer native threads will not be taken into account by the JVM
-         * when deciding whether to shutdown, so we have to keep the main thread
-         * alive. Gst.main() will keep the calling thread alive until Gst.quit()
-         * is called. Here we use the built-in executor to schedule a quit after
-         * 10 seconds.
-         */
-       // Gst.getExecutor().schedule(Gst::quit, 10, TimeUnit.SECONDS);
-        Gst.main();
+        	
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
