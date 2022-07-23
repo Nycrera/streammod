b119908456730f01223364bd01df385834d59586
Alperen Yurdakul <alperenyurdakul7@gmail.com>
2022-07-23 21:00:04 +0300

    Generate jdocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.alperen.streammod","l":"ScreenStreamer"},{"p":"com.alperen.streammod","l":"StreamPlayer"},{"p":"com.alperen.streammod","l":"StreamRecorder"},{"p":"com.alperen.streammod","l":"Util"},{"p":"com.alperen.streammod","l":"VideoStreamer"}]
